b22e93b1a3541f5b3adf863c064db76ad662b0df
Grogu-Dindjarin <grogu.dindjarin9@gmail.com>
2024-03-20 12:45:02 +0100

    Modify Main class
    Initialise instances of Ingredient objects and ApplePieRecipe
    Call upon class methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
+
+        Ingredient unsaltedButter = new Ingredient(200, Unit.GRAM, "ongezouten roomboter");
+        Ingredient whiteCasterSugar = new Ingredient(200, Unit.GRAM, "witte bastard suiker");
+        Ingredient selfRaisingFlour = new Ingredient(400, Unit.GRAM, "zelfrijzend bakmeel");
+        Ingredient egg = new Ingredient(1, Unit.PIECE, "ei");
+        Ingredient vanillaSugar = new Ingredient(8, Unit.GRAM, "vanillesuiker");
+        Ingredient pinchOfSalt = new Ingredient(1, Unit.PINCH, "zout");
+        Ingredient sweetAndSourApples = new Ingredient((int) 1.5, Unit.KILOGRAM, "zoetzure appels");
+        Ingredient granulatedSugar = new Ingredient(75, Unit.GRAM, "kristal suiker");
+        Ingredient cinnamon = new Ingredient(3, Unit.TEASPOONS, "kaneel");
+        Ingredient breadcrumbs = new Ingredient(15, Unit.GRAM, "paneermeel");
+
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+
+        ingredients.add(unsaltedButter);
+        ingredients.add(whiteCasterSugar);
+        ingredients.add(selfRaisingFlour);
+        ingredients.add(egg);
+        ingredients.add(vanillaSugar);
+        ingredients.add(pinchOfSalt);
+        ingredients.add(sweetAndSourApples);
+        ingredients.add(granulatedSugar);
+        ingredients.add(cinnamon);
+        ingredients.add(breadcrumbs);
+
+        applePieRecipe.printIngredients(ingredients);
+        applePieRecipe.printRecipeInstructions();
     }
 }
