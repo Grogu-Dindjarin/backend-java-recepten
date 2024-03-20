86311e18e9347dacd44e9e65dd6588ee516f27ea
Grogu-Dindjarin <grogu.dindjarin9@gmail.com>
2024-03-20 12:41:55 +0100

    Create Enum Unit
    Create enumarable ingredient units

--- src/Unit.java
+++ src/Unit.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public enum Unit {
+public enum Unit {
+    GRAM("gram"),
+    KILOGRAM("kilo"),
+    PIECE("stuk(s)"),
+    TEASPOONS("theelepels"),
+    PINCH("snuf");
+
+    private final String unitName;
+
+    Unit(String unitName) {
+        this.unitName = unitName;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
 }
