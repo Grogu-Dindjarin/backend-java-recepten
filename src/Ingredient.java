0eef52c6b375ef5c6aca2b8b8d11c53232202d45
Grogu-Dindjarin <grogu.dindjarin9@gmail.com>
2024-03-20 12:42:55 +0100

    Create Ingredient class
    Create constructor with arg, default constr and getters and setters

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Ingredient {
+public class Ingredient {
+    private Number amount;
+    private Unit unit;
+    private String name;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(Number amount, Unit unit, String name) {
+        this.amount = amount;
+        this.unit = unit;
+        this.name = name;
+    }
+
+    public Number getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
